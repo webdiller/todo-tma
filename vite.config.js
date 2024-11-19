e2ef71f3d1a00c4d926c52f243695d8613d4737e
webdiller <eugenefromrus@gmail.com>
2024-11-20 08:28:16 +1000

    upd: upd tsconfig

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
+// https://vitejs.dev/config/
+export default defineConfig({
+    server: {
+        port: 3000,
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+            "@public": path.resolve(__dirname, "./public"),
+            "@assets": path.resolve(__dirname, "./src/assets"),
+            "@widgets": path.resolve(__dirname, "./src/widgets"),
+            "@shared": path.resolve(__dirname, "./src/shared"),
+        },
+    },
+    plugins: [
+        react(),
+        TanStackRouterVite(),
+        // splashScreen({
+        //   logoSrc: "vite.svg",
+        // }),
+    ],
+});
